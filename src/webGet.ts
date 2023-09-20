979c73209ffbae7e1c1528614d1f7a2475313729
adasneves127 <adasneves127@gmail.com>
2023-09-20 18:53:13 -0400

    Moving to Python

--- src/webGet.ts
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch';
-import {WebResponse} from "./Utils"
-
-
-export async function getData(url: string): Promise<WebResponse> {
-    return fetch(url).then((res) => { return res.json() as unknown as WebResponse});
-}
