5b262adfba7791ad19f62ffddf814127cb66fc0c
adasneves127 <adasneves127@gmail.com>
2023-10-11 22:44:53 -0400

    Added Selenium Tests

--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,12 @@
 import pytest
 from main import get_pages_past_year, filter_title, get_article_by_id
-from db_conn import db_conn, article_dict
+from db_conn import db_conn, article_dict, comment_dict
+from datetime import datetime
+from app import app
+from threading import Thread
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 
 def test_article_comment_length():
@@ -26,6 +32,72 @@ def test_database_functionality():
     db.insert("articles", {"objectID": 1, "title": "test"})
     results = db.exec_raw("SELECT * FROM articles;")
     assert results[0][0] == 1  # Check that the object ID matches
+
+
+def test_age_filter(tmp_path):
+    f1 = tmp_path / "test.db"
+    db = db_conn(f1)
+    db.create_table("articles", article_dict)
+    db.create_table("comments", comment_dict)
+    db.insert("articles", {
+        "objectID": 1,
+        "created_at": datetime.fromtimestamp(0),
+        "title": "Ask HN: Test"
+    })
+    db.insert("comments", {
+        "id": 2,
+        "parent_id": 1,
+        "company": "Dasneves Data",
+        "location": "Somewhereville, MA",
+        "salary_low": "75000",
+        "salary_high": "100000",
+        "created_at": 1659326400
+    })
+    db.insert("comments", {
+        "id": 3,
+        "parent_id": 1,
+        "company": "Santore Software",
+        "location": "Nowhereville, MA",
+        "salary_low": "100000",
+        "salary_high": "100001",
+        "created_at": 1690862400
+    })
+    date = 1690362400
+
+    res = db.exec_raw(f"select * from comments where created_at > {date}")
+    db.close()
+    # Make sure we get one and only one comment
+    assert len(res) == 1
+    # Make sure we get the correct comment
+    assert res[0][0] == 3
+
+
+def test_gui_placement():
+    # Create a thread for our web app. Daemon says don't wait for process end
+    web_thread = Thread(target=app.run, daemon=True)
+    web_thread.start()  # Start the server
+    db = db_conn('output.db')  # Get an article from the database
+    CID = 33068430
+    query = f"SELECT * FROM comments WHERE ID={CID}"
+    # Strip whitespace from end
+    results = [str(x).strip() for x in db.exec_raw(query)[0]]
+    db.close()
+    # Create a Firefox Selenium Instance
+    options = FirefoxOptions()
+    options.add_argument('--headless')  # No window!
+    driver = webdriver.Firefox(options=options)
+    # Get the site, compare that everything looks good
+    driver.get("localhost:5000/expand/33068430")
+    assert driver.find_element(By.ID, 'CID').text == f'View Comment {CID}'
+    assert driver.find_element(By.ID, 'CompanyName').text ==\
+        f'Company Name: {results[2]}'
+    assert driver.find_element(By.ID, 'Salary').text == \
+        f"Salary Range: ${results[4]}-${results[5]}"
+    assert driver.find_element(By.ID, 'Location').text ==\
+        f"Location: {results[3]}"
+    assert driver.find_element(By.ID, 'Comment').text ==\
+        "Comment:\n" + results[6]
+    driver.close()  # Close the firefox instance
 
 
 if __name__ == "__main__":
